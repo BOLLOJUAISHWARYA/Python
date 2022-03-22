008de20e110742cb11dc2f252266a2cbb4d5c4ee
Aishwarya B <59137401+BOLLOJUAISHWARYA@users.noreply.github.com>
2022-03-22 16:42:14 +0530

    Add files via upload

---
+++ Email.py
@@ -0,0 +1,22 @@
+import smtplib
+
+From = 'aishwarya.bolloju@gmail.com'
+password = "ymfmdgotbmhnottr"
+
+to = "arun.nadar@neosoftmail.com"
+cc = ["sai.uddaraju@neosoftmail.com", "anushka.mhatre@neosoftmail.com"]
+bcc = ["vibha.rawan@neosoftmail.com", "rohan.dhere@neosoftmail.com"]
+subject = 'Email sending Assignment'
+body = "Hi ! this mail is generated using python code--Aishwarya Bolloju"
+
+email = f'To: {to} \rCc: {",".join(cc)} \rBcc :{",".join(bcc)}\nSubject: {subject}\n\n{body}'
+#\r carriage return
+
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls()       #puts the connection to the SMTP server into TLS mode.
+print("trying to login")
+server.login(From, password)
+print("login in successful")
+server.sendmail(From, to, email)
+server.close()
+print("mail sent")
