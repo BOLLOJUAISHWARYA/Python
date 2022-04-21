f2de724373c37bc386654c1f87f0726cd1f77b9d
BOLLOJUAISHWARYA <bolloju.aishwarya@gmail.com>
2022-04-21 20:35:35 +0530

    login_ajax_sqlite

--- login_page/database.py
+++ login_page/database.py
@@ -2,6 +2,6 @@ import sqlite3
 
 conn = sqlite3.connect('database.db')
 print('opened')
-conn.execute(""" CREATE TABLE details(name VARCHAR(255) ,city CHAR(25) ,pin varchar(255),password varchar(255))""")
+conn.execute(""" CREATE TABLE details(id int UNIQUE ,name VARCHAR(255) ,city CHAR(25) ,pin varchar(255),password varchar(255))""")
 print('Table created')
 conn.close()
--- login_page/main.py
+++ login_page/main.py
@@ -15,10 +15,11 @@ def home():
 def new():
     return render_template('new.html')
 
-@app.route('/add',methods=['POST','GET'])
+@app.route('/add',methods=['POST'])
 def add():
     if request.method == 'POST':
         try:
+          id = request.form['id']
           name = request.form['name']
           city = request.form['city']
           pin = request.form['pin']
@@ -26,19 +27,20 @@ def add():
 
           with sql.connect('database.db') as con:
               cur = con.cursor()
-              cur.execute('''INSERT INTO details(name,city,pin,password) VALUES (?,?,?,?)''',(name,city,pin,password))
+              cur.execute('''INSERT INTO details(id,name,city,pin,password) VALUES (?,?,?,?,?)''',(id,name,city,pin,password))
               msg= "Added"
 
         except:
             con.rollback()
-            msg="Error..!couldn't insert"
+            msg = "Error..!couldn't insert"
 
         finally:
-            return render_template("result.html",msg=msg)
+            return msg
+            con.close()
 
-@app.route('/login_page')
+@app.route('/login')
 def login():
-    return render_template('login_page.html')
+    return render_template('login.html')
 
 @app.route('/check',methods=['POST','GET'])
 def check():
@@ -47,10 +49,14 @@ def check():
 
     with sql.connect('database.db') as con:
         cur = con.cursor()
-        c = cur.execute(f"SELECT * from details WHERE name='{name}' AND password='{password}';")
+        c = cur.execute(f"SELECT * from details WHERE name='{name}' AND password='{password}'")
         if not c.fetchone():
             return render_template('new.html')
         else:
-            return "Welcome" + " " +name
+            con.row_factory = sql.Row
+            cur = con.cursor()
+            c = cur.execute(f"SELECT * from details WHERE name='{name}'")
+            rows = c.fetchall()
+            return render_template("result.html",rows=rows)
 
 app.run()
